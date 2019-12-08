60e22d9e0246416e2f453c1d15fe26193e93d39f
Mockba the Borg <MockbaTheBorg@users.noreply.github.com>
2019-12-08 12:57:40 -0500

    Took extra backgrounds off Mac and Linux

--- src/MockbaModular.hpp
+++ src/MockbaModular.hpp
@@ -1,7 +1,11 @@
 #ifndef MOCKBAMODULAR_HPP
 #define MOCKBAMODULAR_HPP
 
-#define BGCOLOR loadBack()
+#ifdef _WIN32
+	#define BGCOLOR loadBack()
+#else
+	#define BGCOLOR "res/Empty_light.svg"
+#endif
 
 void saveBack(const char* Back);
 const char* loadBack();
